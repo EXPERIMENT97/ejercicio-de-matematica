3b33588add7592caba09373f4f04ca18742d9587
EXPERIMENT97 <lucasmuguiro@hotmail.com>
2019-05-28 11:22:43 -0300

    Modificacion y mejora

---
+++ misFunciones.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Lucas on 17/5/2019.
+ */
+
+/**
+ * Descripción
+ * @method Nombre de la función
+ * @param Parámetro A
+ * @param Parámetro B
+ * @return Valor que retorna
+ */
+function saludar_inicio() {
+    alert("Bienevenido a mi pagina. Espero que les guste.")
+}
+function saludar1() {
+    alert("Bienvenido a Segementos y Angulos.")
+}
+function saludar2() {
+    alert("Bienvenido a Area, Perimetro y Volumen")
+}
