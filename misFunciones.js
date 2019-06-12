0e888302983dcded73e7f15d5cc227f1ccc874f4
EXPERIMENT97 <lucasmuguiro@hotmail.com>
2019-06-12 13:59:09 -0300

    Mejoras 12/06_2

--- misFunciones.js
+++ misFunciones.js
@@ -22,16 +22,16 @@ function Atri() {
     document.opeMat.area_tri.value = Number(document.opeMat.A.value) + Number(document.opeMat.B.value) + Number(document.opeMat.C.value);
 }
 function Ptri() {
-    document.opeMat.perimetro_tri.value = (Number(document.opeMat.base.value) * Number(document.opeMat.altura.value));
+    document.opeMat.perimetro_tri.value = (Number(document.opeMat.base.value) * Number(document.opeMat.altura.value)) / 2;
 }
 function Ap() {
-
+    document.opeMat.area_p.value = 2 * (Number(document.opeMat.altura.value) + Number(document.opeMat.base.value));
 }
 function Pp() {
-
+    document.opeMat.perimetro_p.value = Number(document.opeMat.base_pp.value) * Number(document.opeMat.altura_pp.value);
 }
 function Are() {
-
+    document.opeMat.area_re.value = Number(document.opeMat.base.value) * Number(document.opeMat.altura.value);
 }    //document.opeMat.pe.value = 2 *( (Number(document.opeMat.lado_br.value) + Number(document.opeMat.lado_ar.value)));
 
 function Pre() {
@@ -43,25 +43,26 @@ function Pre() {
     //total.value = totalLado;
 }
 function Acua() {
-
+    document.opeMat.area_cua.value = Number(document.opeMat.altura.value)
 }
 function Pcua() {
-
+    document.opeMat.perimetro_cua.value = 4 * Number(document.opeMat.altura_cua.value);
 }
 function Aro() {
-    
+    document.opeMat.area_ro.value = (Number(document.opeMat.Diagonal_mayor_o_lado.value) * Number(document.opeMat.diagonal_menor_o_altura.value)) / 2;
 }
 function Pro() {
-    
-}function Atra() {
-    
+    document.opeMat.perimetro_ro.value = 4 * Number(document.opeMat.altura.value);
+}
+function Atra() {
+    document.opeMat.area_tra.value = Number(document.opeMat.Base_mayor.value) + Number(document.opeMat.base_menor.value) + Number(document.opeMat.lado_c.value) + Number(document.opeMat.lado_d.value);
 }
 function Ptra() {
-    
+    document.opeMat.perimetro_tra.value = ((Number(document.opeMat.Base_mayor.value) + Number(document.opeMat.base_menor.value)) * Number(document.opeMat.altura.value)) / 2;
 }
 function Acir() {
-    
+    //document.opeMat.area_cir.value = Number(document.opeMat..value) * Number(document.opeMat.radio.value) ^ 2;
 }
 function Pcir() {
-    
+    //document.opeMat.perimetro_cir.value = 2* Number(document.opeMat..value) * Number(document.opeMat.radio.value);
 }
